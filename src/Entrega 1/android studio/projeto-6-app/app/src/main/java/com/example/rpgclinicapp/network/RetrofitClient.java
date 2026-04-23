5371925c47aaa5671444bea0c14a7c80df039985
Rafael Chagas Silva <25027262@fecap.br>
2026-04-23 20:10:27 -0300

    Atualização retrofit

--- src/Entrega 1/android studio/projeto-6-app/app/src/main/java/com/example/rpgclinicapp/network/RetrofitClient.java
+++ src/Entrega 1/android studio/projeto-6-app/app/src/main/java/com/example/rpgclinicapp/network/RetrofitClient.java
@@ -9,7 +9,7 @@ import java.util.concurrent.TimeUnit;
 public class RetrofitClient {
 
     // URL do backend no CodeSandbox (Certifique-se que o projeto lá está como PUBLIC)
-    private static final String BASE_URL = "https://lnyv32-3000.csb.app/";
+    private static final String BASE_URL = "https://73wl5m-3000.csb.app/";
 
     private static Retrofit retrofit = null;
 
